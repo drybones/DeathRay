4abbd683bbb707e921a266452bd68d5f04128f0e
David Bick <david@drybones.co.uk>
2014-07-18 17:05:35 +0100

    ...and put everything back.

---
+++ DeathRay/Controllers/MinionWorkController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeathRay.Controllers
+{
+    [Authorize]
+    [RequireHttps]
+    public class MinionWorkController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DeathRay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeathRay.Startup))]
+namespace DeathRay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
