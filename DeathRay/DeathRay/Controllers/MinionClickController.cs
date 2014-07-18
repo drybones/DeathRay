a5af21b116fca9596ca47026e23a15bb4b58d09c
David Bick <david@drybones.co.uk>
2014-07-18 16:54:05 +0100

    Initial commit
    Basic app with account database and Google login

---
+++ DeathRay/DeathRay/Controllers/MinionClickController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+using DeathRay.Models;
+
+namespace DeathRay.Controllers
+{
+    public class MinionClickController : Controller
+    {
+        // GET: MinionClick
+        [Authorize]
+        [RequireHttps]
+        public ActionResult Click()
+        {
+            var minionClick = new MinionClick() { Minion = User.Identity.GetUserName(), Timestamp = DateTime.Now };
+            return Json(minionClick);
+        }
+    }
+}
